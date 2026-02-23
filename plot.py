2171dff58ddbf0e457e2a0a0e8ee7389a295ac6f
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2026-02-23 10:06:50 +0100

    Add new plots

--- plot.py
+++ plot.py
@@ -23,6 +23,11 @@ def _():
 @app.cell
 def _(pd):
     df = pd.read_csv("perf", sep=" ")
+    # Add CPU time
+    df["cputime_in_sec"] = df["runtime_in_sec"] * df["thread"]
+    # Remove NaN columns
+    df = df.drop('RPU_cutoff', axis=1)
+    df = df.drop('RPU_cutoff_model', axis=1)
     return (df,)
 
 
@@ -35,7 +40,7 @@ def _(df, mo):
         ("umi_len", int),
         ("insertion-deletion-substitution", str),
         ("err_rate", float),
-    
+
     ]
     filter = mo.ui.dictionary(
         { field: mo.ui.dropdown(options=df[field], value=type(df[field][0])) for field, type in fields}
@@ -61,9 +66,6 @@ def _(df, fields, filter):
         all_true.append(a)
 
     filtered=df.loc[all_true]
-    # Remove NaN columns
-    filtered = filtered.drop('RPU_cutoff', axis=1)
-    filtered = filtered.drop('RPU_cutoff_model', axis=1)
     return (filtered,)
 
 
@@ -75,15 +77,15 @@ def _(filter):
 
 @app.cell
 def _(alt, filtered):
-    def barchart(df, column, log=False):
+    def barchart(df, column, log=False, domain=None):
         #column = "V-measure"
         #column = "runtime_in_sec"
         # The bars
         bars=alt.Chart(df).mark_bar().encode(
             x='tool',
-            y=alt.Y(f'mean({column}):Q').title(f'Mean {column}').scale(type="log" if log else "", domain =[0.9,1]),
+            y=alt.Y(f'mean({column}):Q').title(f'Mean {column}').scale(type="log" if log else "", domain=None),
             color=alt.Color("tool")
-    
+
         )
         # The error bars
         error_bars = alt.Chart().mark_errorbar(extent='ci').encode(
@@ -92,8 +94,51 @@ def _(alt, filtered):
         )
         return alt.layer(bars,error_bars, data=df)
 
-    barchart(filtered, "V-measure")
-    #barchart(filtered, "runtime_in_sec")
+    barchart(filtered, "V-measure", domain =[0.9,1])
+    return (barchart,)
+
+
+@app.cell
+def _(barchart, filtered):
+    barchart(filtered, "runtime_in_sec")
+    return
+
+
+@app.cell
+def _(barchart, filtered):
+    barchart(filtered, "cputime_in_sec")
+    return
+
+
+@app.cell
+def _(mo):
+    settings = mo.ui.dictionary({
+        "field": mo.ui.dropdown(options=["V-measure", "runtime_in_sec", "cputime_in_sec"], value="cputime_in_sec"),
+        "mutation": mo.ui.dropdown(options=["1-1-1", "1-1-40"], value="1-1-1"),
+        "founders": mo.ui.dropdown(options=[1000, 10000], value=1000),
+    })
+    settings
+    return (settings,)
+
+
+@app.cell
+def _(alt, df, settings):
+    field=settings.value["field"]
+    founders=settings.value["founders"]
+
+    t = df.loc[df["insertion-deletion-substitution"] == settings.value["mutation"]]
+    t = t.loc[t["num_founder"] == settings.value["founders"]]
+
+    size=200
+
+    alt.Chart(t, width=size, height=size).mark_bar().encode(
+        x="tool",
+        y=f"mean({field}):Q",
+        color=alt.Color("tool"),
+        row="err_rate",
+        column="umi_len",
+        tooltip=[field, "insertion-deletion-substitution"]
+    ).properties(title=f"Metrics for simulated data with {founders} original reads and 200 children")
     return
 
 
